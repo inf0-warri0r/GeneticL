60643f3efb0d1cfc0cae25e7c7b4796ff5f2abae
tharindra galahena <tcg.galahena@hmail.com>
2013-05-08 20:32:48 +0530

    c3

--- l_system.py
+++ l_system.py
@@ -1,22 +1,22 @@
 """
 Author : tharindra galahena (inf0_warri0r)
-Project: L-system
+Project: GeneticL - L-System generator using genetic algorithms
 Blog   : http://www.inf0warri0r.blogspot.com
-Date   : 30/04/2013
+Date   : 08/05/2013
 License:
 
      Copyright 2013 Tharindra Galahena
 
-This is free software: you can redistribute it and/or modify it under
+GeneticL is free software: you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
 Foundation, either version 3 of the License, or (at your option) any later
-version. This is distributed in the hope that it will be useful, but
+version. GeneticL is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
 details.
 
 * You should have received a copy of the GNU General Public License along with
-this. If not, see http://www.gnu.org/licenses/.
+GeneticL. If not, see http://www.gnu.org/licenses/.
 
 """
 
